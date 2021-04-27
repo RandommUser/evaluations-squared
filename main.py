53df0bf627603de2ecb9fba8fdad0bb0c2788b2c
Petrus Hakakoski <phakakos@student.hive.fi>
2021-04-28 00:05:06 +0300

    Added example functions

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
+#!/bin/python3
 # global
 import sys
 # local
 import database, helpers, database_func, api42
 
+# Basic commands
+"""
+	# print students table for the campus
+	students = database_func.select_from_table(connection, "students", database.keys['students'], (("campus_id =",)), (("13",)))
+	helpers.print_db(students, database.keys['students'])
+	# print the projects and their average times
+	projects = database_func.select_from_table(connection, "projects", (("name", "average_time")), (("campus_id =",)), ((database.setup['campus'],)))
+	helpers.print_db(projects, (("name", "average_time")), False)
+
+# print student table
+students = database_func.select_from_table(connection, "students", database.keys["students"], (("campus_id",)), ((database.setup['campus'],)))
+helpers.print_db(students, database.keys["students"])
+
+# projects and their average eval time
+projects = database_func.select_from_table(connection, "projects", (("name", "average_time")), (("campus_id",)), ((database.setup['campus'],)))
+helpers.print_db(students, (("name", "average_time")), False)
+
+"""
+
 def main():
 	try:
-		token = api42.get_token()
-		connection = database.establish()
-		database.setup_db(connection, token)
+	token = api42.get_token()
+	connection = database.establish()
+	database.setup_db(connection, token)
 	except:
 		print("Error")
+	finally:
 		if connection != None:
 			connection.close()
-	finally:
 		exit()
 
 if __name__ == '__main__':
